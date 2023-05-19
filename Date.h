7a44dd3888ca6677e61faae80cd7445396bf89c2
geppo <andreagepponi1@stud.unifi.it>
2023-05-19 16:40:29 +0200

    Nuova classe Date

---
+++ Date.h
@@ -0,0 +1,39 @@
+//
+// Created by geppo on 19/05/2023.
+//
+
+#ifndef ANDREAGEPPONILAB_DATE_H
+#define ANDREAGEPPONILAB_DATE_H
+#include <string>
+class Date{
+public:
+    Date()=default;
+
+    int getYear() const {
+        return Year;
+    }
+
+    void setYear(int year) {
+        Year = year;
+    }
+
+    int getMonth() const {
+        return Month;
+    }
+
+    void setMonth(int month) {
+        Month = month;
+    }
+
+    int getDay() const {
+        return Day;
+    }
+
+    void setDay(int day) {
+        Day = day;
+    }
+
+private:
+    int Year,Month,Day;
+};
+#endif //ANDREAGEPPONILAB_DATE_H
